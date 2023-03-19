0b2c56cb86000ef53608c8535ae59b2bb55490af
knrdl <35548889+knrdl@users.noreply.github.com>
2023-03-19 21:11:15 +0100

    fix: support static background images from web

--- src/routes/settings/background/+page.svelte
+++ src/routes/settings/background/+page.svelte
@@ -1,90 +1,106 @@
 <script lang="ts">
-    import {applyAction, deserialize, enhance} from '$app/forms';
-    import Fa from "svelte-fa";
-    import {faDownload, faInfoCircle, faRotate} from "@fortawesome/free-solid-svg-icons";
-    import BackgroundRule from "./BackgroundEditor.svelte";
-    import SaveButton from "../SaveButton.svelte";
-    import {invalidateAll} from "$app/navigation";
-    import Message from "../Message.svelte";
-    import cloneDeep from 'lodash.clonedeep'
+	import { applyAction, deserialize, enhance } from '$app/forms';
+	import Fa from 'svelte-fa';
+	import { faDownload, faInfoCircle, faRotate } from '@fortawesome/free-solid-svg-icons';
+	import BackgroundRule from './BackgroundEditor.svelte';
+	import SaveButton from '../SaveButton.svelte';
+	import { invalidateAll } from '$app/navigation';
+	import Message from '../Message.svelte';
+	import cloneDeep from 'lodash.clonedeep';
 
-    export let form;
-    export let data;
+	export let form;
+	export let data;
 
-    async function handleSubmit(event) {
-        const body = new FormData()
-        body.set('userConfig', JSON.stringify(data.userConfig))
+	async function handleSubmit(event) {
+		const body = new FormData();
+		body.set('userConfig', JSON.stringify(data.userConfig));
 
-        const response = await fetch(this.action, {method: 'POST', body})
-        const result = deserialize(await response.text())
-        if (result.type === 'success') {
-            await invalidateAll()
-        }
-        applyAction(result)
-    }
+		const response = await fetch(this.action, { method: 'POST', body });
+		const result = deserialize(await response.text());
+		if (result.type === 'success') {
+			await invalidateAll();
+		}
+		applyAction(result);
+	}
 
-    function cloneCfg(idx, config) {
-        data.userConfig.backgrounds.splice(idx + 1, 0, {...cloneDeep(config), selected: false})
-        data.userConfig.backgrounds = data.userConfig.backgrounds
-    }
+	function cloneCfg(idx, config) {
+		data.userConfig.backgrounds.splice(idx + 1, 0, { ...cloneDeep(config), selected: false });
+		data.userConfig.backgrounds = data.userConfig.backgrounds;
+	}
 
-    function selectCfg(config) {
-        data.userConfig.backgrounds.forEach(bg => bg.selected = false)
-        config.selected = true
-        data.userConfig.backgrounds = data.userConfig.backgrounds
-    }
+	function selectCfg(config) {
+		data.userConfig.backgrounds.forEach((bg) => (bg.selected = false));
+		config.selected = true;
+		data.userConfig.backgrounds = data.userConfig.backgrounds;
+	}
 
-    function deleteCfg(idx) {
-        const wasSelected = data.userConfig.backgrounds[idx].selected
-        data.userConfig.backgrounds.splice(idx, 1)
-        data.userConfig.backgrounds = data.userConfig.backgrounds
-        if (wasSelected)
-            data.userConfig.backgrounds[Math.max(idx - 1, 0)].selected = true
-    }
+	function deleteCfg(idx) {
+		const wasSelected = data.userConfig.backgrounds[idx].selected;
+		data.userConfig.backgrounds.splice(idx, 1);
+		data.userConfig.backgrounds = data.userConfig.backgrounds;
+		if (wasSelected) data.userConfig.backgrounds[Math.max(idx - 1, 0)].selected = true;
+	}
+
+	$: hasRandomBgImg =
+		data.userConfig.backgrounds.find((bg) => bg.selected)?.background === 'random';
 </script>
 
-<Message text={form?.message} kind="success"/>
+<Message text={form?.message} kind="success" />
 
 <div>
-    {#if data.background?.image?.url}
-        <p class="heading">Current random image</p>
-        <div class="flex items-center mb-5">
-            <form method="POST" use:enhance>
-                <div class="flex gap-2">
-                    <button type="submit" formaction="?/reload-random-background-image" class="icon-button">
-                        <Fa icon={faRotate}/>
-                        Shuffle
-                    </button>
-                </div>
-            </form>
-            <a href={data.background.image.url} target="_blank" rel="noopener noreferrer" class="icon-button">
-                <Fa icon={faDownload}/>
-                Download
-            </a>
-        </div>
-    {/if}
+	{#if hasRandomBgImg}
+		<p class="heading">Current random image</p>
+		<div class="flex items-center mb-5">
+			<form method="POST" use:enhance>
+				<div class="flex gap-2">
+					<button type="submit" formaction="?/reload-random-background-image" class="icon-button">
+						<Fa icon={faRotate} />
+						Shuffle
+					</button>
+				</div>
+			</form>
+			{#if data.background?.image?.url}
+				<a
+					href={data.background.image.url}
+					target="_blank"
+					rel="noopener noreferrer"
+					class="icon-button"
+				>
+					<Fa icon={faDownload} />
+					Download
+				</a>
+			{/if}
+		</div>
+	{/if}
 
-    <p class="heading">Your Backgrounds</p>
-    {#each data.userConfig.backgrounds as config, idx}
-        <BackgroundRule {config} {data}
-                        on:clone={()=>cloneCfg(idx, config)}
-                        on:select={()=>selectCfg(config)}
-                        on:delete={()=>deleteCfg(idx)}
-        />
-    {/each}
+	<p class="heading">Your Backgrounds</p>
+	{#each data.userConfig.backgrounds as config, idx}
+		<BackgroundRule
+			{config}
+			{data}
+			on:clone={() => cloneCfg(idx, config)}
+			on:select={() => selectCfg(config)}
+			on:delete={() => deleteCfg(idx)}
+		/>
+	{/each}
 </div>
 
-<form method="POST" on:submit|preventDefault={handleSubmit} action="?/save" class="flex justify-end">
-    <SaveButton/>
+<form
+	method="POST"
+	on:submit|preventDefault={handleSubmit}
+	action="?/save"
+	class="flex justify-end"
+>
+	<SaveButton />
 </form>
 
 <style>
-    div .heading {
-        @apply text-lg font-semibold text-gray-900 dark:text-gray-300;
-    }
+	div .heading {
+		@apply text-lg font-semibold text-gray-900 dark:text-gray-300;
+	}
 
-    div .icon-button {
-        @apply flex justify-center gap-1 p-3;
-        @apply text-white bg-blue-700/80 hover:bg-blue-800 font-medium rounded-lg text-sm p-1 text-center inline-flex items-center mr-2 dark:bg-blue-600/80 dark:hover:bg-blue-700;
-    }
+	div .icon-button {
+		@apply flex justify-center gap-1 p-3;
+		@apply text-white bg-blue-700/80 hover:bg-blue-800 font-medium rounded-lg text-sm p-1 text-center inline-flex items-center mr-2 dark:bg-blue-600/80 dark:hover:bg-blue-700;
+	}
 </style>
--- src/routes/settings/background/BackgroundEditor.svelte
+++ src/routes/settings/background/BackgroundEditor.svelte
@@ -1,172 +1,188 @@
 <script lang="ts">
-    import Fa from "svelte-fa"
-    import {faClone, faSquare, faSquareCheck, faTrashCan} from "@fortawesome/free-solid-svg-icons"
-    import {createEventDispatcher} from "svelte"
-    import {fade} from 'svelte/transition';
+	import Fa from 'svelte-fa';
+	import { faClone, faSquare, faSquareCheck, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+	import { createEventDispatcher } from 'svelte';
+	import { fade } from 'svelte/transition';
 
-    const dispatch = createEventDispatcher()
+	const dispatch = createEventDispatcher();
 
-    export let data
-    export let config: BackgroundConfig
+	export let data;
+	export let config: BackgroundConfig;
 
-    const RandomImageChangeDurations = [
-        {title: 'Never', value: 0},
-        {title: '10m', value: 10 * 60},
-        {title: '30m', value: 30 * 60},
-        {title: '1h', value: 60 * 60},
-        {title: '6h', value: 6 * 60 * 60},
-        {title: '12h', value: 12 * 60 * 60},
-        {title: '24h', value: 24 * 60 * 60},
-    ]
+	const RandomImageChangeDurations = [
+		{ title: 'Never', value: 0 },
+		{ title: '10m', value: 10 * 60 },
+		{ title: '30m', value: 30 * 60 },
+		{ title: '1h', value: 60 * 60 },
+		{ title: '6h', value: 6 * 60 * 60 },
+		{ title: '12h', value: 12 * 60 * 60 },
+		{ title: '24h', value: 24 * 60 * 60 }
+	];
 </script>
 
-<div class="bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 divide-x divide-dashed divide-slate-600 my-3 flex"
-     transition:fade>
-    {#if data.userConfig.backgrounds.length > 1}
-        <div class="flex flex-col justify-center gap-2 p-1">
-            {#if config.selected}
-                <button disabled class="icon-button">
-                    <Fa icon={faSquareCheck}/>
-                    <span>Select</span>
-                </button>
-            {:else}
-                <button on:click={()=>dispatch('select')} class="icon-button">
-                    <Fa icon={faSquare}/>
-                    <span>Select</span>
-                </button>
-            {/if}
-        </div>
-    {/if}
-    <div class="font-normal text-gray-700 dark:text-gray-400 px-3 py-4 grow">
-        <div class="mb-2">
-            <label>
-                • Background:
-                <select bind:value={config.background}>
-                    <option value="triangles">Triangles</option>
-                    <option value="static">Static Image</option>
-                    <option value="random">Random Image</option>
-                </select>
-            </label>
-            {#if config.background === 'static'}
-                <label>
-                    <span>from</span>
-                    <select bind:value={config.static_image.source}>
-                        <!--                        <option value="upload">Upload</option> TODO-->
-                        <option value="web">Web</option>
-                    </select>
-                </label>
-            {/if}
-            {#if config.background === 'random'}
-                <label>
-                    <span>from</span>
-                    <select bind:value={config.random_image.provider}>
-                        <option value="unsplash">Unsplash photo library</option>
-                        <option value="reddit">Reddit posts</option>
-                    </select>
-                </label>
-            {/if}
-        </div>
-        <div class="ml-5 mb-4">
-            {#if config.background === 'random'}
-                {#if config.random_image.provider === 'unsplash'}
-                    <label>
-                        Query:
-                        <input type="text" placeholder="e.g. beach"
-                               bind:value={config.random_image.unsplash_query}/>
-                    </label>
-                {:else if config.random_image.provider === 'reddit'}
-                    <label>
-                        Subreddits:
-                        <input type="text" placeholder="e.g. wallpaper,wallpapers"
-                               bind:value={config.random_image.subreddits}/>
-                    </label>
-                {/if}
-                <label>
-                    Change after:
-                    <select bind:value={config.random_image.duration}>
-                        {#each RandomImageChangeDurations as duration}
-                            <option value={duration.value}>{duration.title}</option>
-                        {/each}
-                    </select>
-                </label>
-            {:else if config.background === 'static'}
-                {#if config.static_image.source === 'upload'}
-                    <label>
-                        <input type="file"/>
-                    </label>
-                {/if}
-                {#if config.static_image.source === 'web'}
-                    <label>
-                        <input type="url" placeholder="please enter image url"/>
-                    </label>
-                {/if}
-            {/if}
-        </div>
+<div
+	class="bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 divide-x divide-dashed divide-slate-600 my-3 flex"
+	transition:fade
+>
+	{#if data.userConfig.backgrounds.length > 1}
+		<div class="flex flex-col justify-center gap-2 p-1">
+			{#if config.selected}
+				<button disabled class="icon-button">
+					<Fa icon={faSquareCheck} />
+					<span>Select</span>
+				</button>
+			{:else}
+				<button on:click={() => dispatch('select')} class="icon-button">
+					<Fa icon={faSquare} />
+					<span>Select</span>
+				</button>
+			{/if}
+		</div>
+	{/if}
+	<div class="font-normal text-gray-700 dark:text-gray-400 px-3 py-4 grow">
+		<div class="mb-2">
+			<label>
+				• Background:
+				<select bind:value={config.background}>
+					<option value="triangles">Triangles</option>
+					<option value="static">Static Image</option>
+					<option value="random">Random Image</option>
+				</select>
+			</label>
+			{#if config.background === 'static'}
+				<label>
+					<span>from</span>
+					<select bind:value={config.static_image.source}>
+						<option value="upload">Upload</option>
+						<option value="web">Web</option>
+					</select>
+				</label>
+			{/if}
+			{#if config.background === 'random'}
+				<label>
+					<span>from</span>
+					<select bind:value={config.random_image.provider}>
+						<option value="unsplash">Unsplash photo library</option>
+						<option value="reddit">Reddit posts</option>
+					</select>
+				</label>
+			{/if}
+		</div>
+		<div class="ml-5 mb-4">
+			{#if config.background === 'random'}
+				{#if config.random_image.provider === 'unsplash'}
+					<label>
+						Query:
+						<input
+							type="text"
+							placeholder="e.g. beach"
+							bind:value={config.random_image.unsplash_query}
+						/>
+					</label>
+				{:else if config.random_image.provider === 'reddit'}
+					<label>
+						Subreddits:
+						<input
+							type="text"
+							placeholder="e.g. wallpaper,wallpapers"
+							bind:value={config.random_image.subreddits}
+						/>
+					</label>
+				{/if}
+				<label>
+					Change after:
+					<select bind:value={config.random_image.duration}>
+						{#each RandomImageChangeDurations as duration}
+							<option value={duration.value}>{duration.title}</option>
+						{/each}
+					</select>
+				</label>
+			{:else if config.background === 'static'}
+				{#if config.static_image.source === 'upload'}
+					<label>
+						<input type="file" />
+					</label>
+				{/if}
+				{#if config.static_image.source === 'web'}
+					<label class="flex">
+						<input type="url" class="grow" placeholder="please enter image url" bind:value={config.static_image.web_url} />
+					</label>
+				{/if}
+			{/if}
+		</div>
 
-        • Filters:
-        <label class="text-sm text-gray-900 dark:text-gray-300">
-            <input checked={config.blur !== false}
-                   on:change={e => config.blur = e.target.checked ? 'dark' : false}
-                   type="checkbox" value="">
-            {#if config.blur === false}
-                Blur
-            {:else}
-                Blur:
-                {#if config.blur}
-                    <select bind:value={config.blur}>
-                        <option value="dark">dark</option>
-                        <option value="light">light</option>
-                    </select>
-                {/if}
-            {/if}
-        </label>
-        <label class="ml-2 text-sm text-gray-900 dark:text-gray-300">
-            <input bind:checked={config.dots} type="checkbox" value="">
-            Dot Matrix
-        </label>
-        <label class="text-sm text-gray-900 dark:text-gray-300">
-            <input checked={config.particles !== false}
-                   on:change={e => config.particles = e.target.checked ? data.particleList[0] : false}
-                   type="checkbox" value="">
-            {#if config.particles && data?.particleList}
-                Particles:
-                <select bind:value={config.particles}>
-                    {#each data.particleList as particle}
-                        <option value={particle}>{particle}</option>
-                    {/each}
-                </select>
-            {:else }
-                Particles
-            {/if}
-        </label>
-    </div>
-    <div class="flex flex-col justify-center gap-2 p-1">
-        <button on:click={()=>dispatch('clone')} class="icon-button">
-            <Fa icon={faClone}/>
-            <span>Copy</span>
-        </button>
-        {#if data.userConfig.backgrounds.length > 1}
-            <button on:click={()=>dispatch('delete')} class="icon-button">
-                <Fa icon={faTrashCan}/>
-                <span>Delete</span>
-            </button>
-        {/if}
-    </div>
+		• Filters:
+		<label class="text-sm text-gray-900 dark:text-gray-300">
+			<input
+				checked={config.blur !== false}
+				on:change={(e) => (config.blur = e.target.checked ? 'dark' : false)}
+				type="checkbox"
+				value=""
+			/>
+			{#if config.blur === false}
+				Blur
+			{:else}
+				Blur:
+				{#if config.blur}
+					<select bind:value={config.blur}>
+						<option value="dark">dark</option>
+						<option value="light">light</option>
+					</select>
+				{/if}
+			{/if}
+		</label>
+		<label class="ml-2 text-sm text-gray-900 dark:text-gray-300">
+			<input bind:checked={config.dots} type="checkbox" value="" />
+			Dot Matrix
+		</label>
+		<label class="text-sm text-gray-900 dark:text-gray-300">
+			<input
+				checked={config.particles !== false}
+				on:change={(e) => (config.particles = e.target.checked ? data.particleList[0] : false)}
+				type="checkbox"
+				value=""
+			/>
+			{#if config.particles && data?.particleList}
+				Particles:
+				<select bind:value={config.particles}>
+					{#each data.particleList as particle}
+						<option value={particle}>{particle}</option>
+					{/each}
+				</select>
+			{:else}
+				Particles
+			{/if}
+		</label>
+	</div>
+	<div class="flex flex-col justify-center gap-2 p-1">
+		<button on:click={() => dispatch('clone')} class="icon-button">
+			<Fa icon={faClone} />
+			<span>Copy</span>
+		</button>
+		{#if data.userConfig.backgrounds.length > 1}
+			<button on:click={() => dispatch('delete')} class="icon-button">
+				<Fa icon={faTrashCan} />
+				<span>Delete</span>
+			</button>
+		{/if}
+	</div>
 </div>
 
-
 <style>
-    /*label required, otherwise colliding with global rules?*/
-    label input[type=text], label input[type=url], label input[type=file], label select {
-        @apply bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white;
-    }
+	/*label required, otherwise colliding with global rules?*/
+	label input[type='text'],
+	label input[type='url'],
+	label input[type='file'],
+	label select {
+		@apply bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg p-1 dark:bg-gray-900 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white;
+	}
 
-    div .icon-button {
-        @apply flex flex-col justify-center gap-1 w-full;
-        @apply text-white bg-blue-700/80 hover:bg-blue-800 font-medium rounded-lg text-sm p-1 text-center inline-flex items-center mr-2 dark:bg-blue-600/80 dark:hover:bg-blue-700;
-    }
+	div .icon-button {
+		@apply flex flex-col justify-center gap-1 w-full;
+		@apply text-white bg-blue-700/80 hover:bg-blue-800 font-medium rounded-lg text-sm p-1 text-center inline-flex items-center mr-2 dark:bg-blue-600/80 dark:hover:bg-blue-700;
+	}
 
-    div .icon-button[disabled] {
-        @apply bg-gray-700 hover:bg-gray-700;
-    }
+	div .icon-button[disabled] {
+		@apply bg-gray-700 hover:bg-gray-700;
+	}
 </style>
