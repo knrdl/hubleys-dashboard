51a70870d097f2cbf95ff526f3d2e97b785c4e6c
knrdl <35548889+knrdl@users.noreply.github.com>
2023-12-29 15:51:11 +0100

    fix: animation on tile folder was broken by outer if condition

--- src/routes/TileFolder.svelte
+++ src/routes/TileFolder.svelte
@@ -32,37 +32,35 @@
 
 <svelte:window bind:innerWidth={windowWidth} bind:innerHeight={windowHeight} on:keydown={keydown} />
 
-{#if menu}
-  <button
-    type="button"
-    class="fixed bottom-0 left-0 right-0 top-0 z-[1000] block h-screen w-screen cursor-default"
-    on:click|stopPropagation={() => dispatch('close')}
-    on:keydown={keydown}
+<button
+  type="button"
+  class="fixed bottom-0 left-0 right-0 top-0 z-[1000] block h-screen w-screen cursor-default"
+  on:click|stopPropagation={() => dispatch('close')}
+  on:keydown={keydown}
+>
+  <div
+    class="fixed rounded-lg border border-gray-200 bg-gray-100/75 p-3 shadow-xl dark:border-gray-700 dark:bg-gray-900/95"
+    bind:clientWidth={dialogWidth}
+    bind:clientHeight={dialogHeight}
+    style="left: {leftPos}px; top: {topPos}px"
+    transition:scale={{ duration: 200 }}
   >
-    <div
-      class="fixed rounded-lg border border-gray-200 bg-gray-100/75 p-3 shadow-xl dark:border-gray-700 dark:bg-gray-900/95"
-      bind:clientWidth={dialogWidth}
-      bind:clientHeight={dialogHeight}
-      style="left: {leftPos}px; top: {topPos}px"
-      transition:scale={{ duration: 200 }}
-    >
-      <div class="mb-2">
-        <h1 class="select-none text-center text-2xl text-gray-900 dark:text-gray-100">
-          {menu.title}
-        </h1>
-        {#if menu.subtitle}
-          <h2 class="select-none text-center text-lg text-gray-900 dark:text-gray-100">
-            {menu.subtitle}
-          </h2>
-        {/if}
-      </div>
-      <div class="inline-grid gap-1" class:grid-cols-2={menu.tiles.length > 1} class:grid-rows-2={menu.tiles.length > 2}>
-        {#each menu.tiles || [] as menuTile, idx}
-          <div class:col-span-2={idx === menu.tiles.length - 1 && idx % 2 === 0} class="flex justify-center">
-            <TileComponent {...menuTile} />
-          </div>
-        {/each}
-      </div>
+    <div class="mb-2">
+      <h1 class="select-none text-center text-2xl text-gray-900 dark:text-gray-100">
+        {menu?.title || ''}
+      </h1>
+      {#if menu?.subtitle}
+        <h2 class="select-none text-center text-lg text-gray-900 dark:text-gray-100">
+          {menu?.subtitle || ''}
+        </h2>
+      {/if}
     </div>
-  </button>
-{/if}
+    <div class="inline-grid gap-1" class:grid-cols-2={(menu?.tiles.length || 0) > 1} class:grid-rows-2={(menu?.tiles.length || 0) > 2}>
+      {#each menu?.tiles || [] as menuTile, idx}
+        <div class:col-span-2={idx === (menu?.tiles.length || 0) - 1 && idx % 2 === 0} class="flex justify-center">
+          <TileComponent {...menuTile} />
+        </div>
+      {/each}
+    </div>
+  </div>
+</button>
