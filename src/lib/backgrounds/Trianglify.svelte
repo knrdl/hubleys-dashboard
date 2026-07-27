4eafbbaa435d0f452a939f6fc2b765ac639dcdca
knrdl <35548889+knrdl@users.noreply.github.com>
2026-07-27 09:29:27 +0000

    style: fix

--- src/lib/backgrounds/Trianglify.svelte
+++ src/lib/backgrounds/Trianglify.svelte
@@ -7,7 +7,7 @@
   onMount(async () => {
     const pattern = trianglify({ width: window.innerWidth, height: window.innerHeight })
     pattern.toCanvas(triangleCanvas)
-    // trianglify sets CSS width/height on large screens which prevents resizes 
+    // trianglify sets CSS width/height on large screens which prevents resizes
     triangleCanvas.style.width = ''
     triangleCanvas.style.height = ''
   })
